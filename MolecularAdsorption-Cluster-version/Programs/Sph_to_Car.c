c5739e6f318edf292f2ed7d7853c6aa0bec996e0
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-06-03 15:41:47 -0500

    Update Sph_to_Car.c

--- MolecularAdsorption-Cluster-version/Programs/Sph_to_Car.c
+++ MolecularAdsorption-Cluster-version/Programs/Sph_to_Car.c
@@ -1,3 +1,4 @@
+#include<time.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
@@ -5,14 +6,23 @@
 //0=x,  1=y, 2=z; 0=r,1=theta, 2=phi
 float r, theta, phi;
 float x,y,z;
+float epsilon, delta, M, N;
 int Ntheta, Nphi;
 int a, b;
 int main()
 {
-
+srand (time(NULL));
 scanf("%f %i %i %i %i",&r,&a,&b, &Ntheta, &Nphi);
-theta=(3.1415926535*a/(Ntheta+1));
-phi=(2*3.1415926535*b/Nphi);
+// epsilon y delta son el elemento aleatorio del algoritmo
+N=(3.1415926535/(2*(Ntheta)));
+M=-N;
+epsilon=M+(float)rand()/((float)RAND_MAX / (N-M));
+N=(3.1415926535/(Nphi));
+M=0-N;
+delta=M+(float)rand()/((float)RAND_MAX / ( N-M) );
+
+theta=(3.1415926535*a/(Ntheta))+epsilon;
+phi=(2*3.1415926535*b/Nphi)+delta;
 
 x=(r*sin(theta)*cos(phi));
 y=(r*sin(theta)*sin(phi));
@@ -22,3 +32,4 @@ z=(r*cos(theta));
 printf("%f %f %f\n", x, y, z);
 return 0;
 }
+
